feb1e9f6124155ca3cafdceacd4771bcbf4e891f
ScorchedTrace <Songkhla0@gmail.com>
2021-12-28 20:20:41 +0700

    Attempt to make interactable script
    - Moved some scripts to script folder
    - Wrote codes from tutorials

--- Lunar Hunt/Assets/Scripts/Interactable.cs
+++ Lunar Hunt/Assets/Scripts/Interactable.cs
@@ -1,9 +1,28 @@
 ﻿using UnityEngine;
 
+//INTERACTION - Making an RPG in Unity (E02)
+//https://www.youtube.com/watch?v=9tePzyL6dgc&ab_channel=Brackeys
+
+/*
+This component is for all objects that the player can
+interact with such as enemies, items etc. It is meant
+to be used as a base class.
+*/
+
 public class Interactable : MonoBehaviour
 {
-    public float radius = 3f;
+    public float radius = 3f; // How close do we need to be to interact
+    public Transform interactionTransform; // The transform from where we interact
+    bool isFocus = false; // Is this interactable currently being focused?
+    Transform player; // Reference to the player transform
 
+    bool hasInteracted = false; // Have we already interacted with the object?
+
+    public virtual void Interact()
+    {
+        // This method is meant to be overwritten
+        Debug.Log("Interacting with " + transform.name);
+    }
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
---
+++ Lunar Hunt/Assets/Scripts/Inventory.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//ITEMS - Making an RPG in Unity (E04)
+//https://www.youtube.com/watch?v=HQNl3Ff2Lpo&ab_channel=Brackeys
+//7:03
+
+//Might have to learn about "Singleton Patterns"
+
+public class Inventory : MonoBehaviour
+{
+    #region Singleton
+
+    public static Inventory instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("More than one instance of Inventory found!");
+            return;
+        }
+        instance = this;
+    }
+
+    #endregion
+
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+
+    public int space = 20;
+
+    public List<Item> items = new List<Item>();
+    public bool Add(Item item)
+    {
+        if (!item.isDefaultItem)
+        {
+            if (items.Count >= space)
+            {
+                Debug.Log("Not enough room.");
+                return false;
+            }
+
+            items.Add(item);
+
+            if (onItemChangedCallback != null)
+                onItemChangedCallback.Invoke();
+        }
+
+        return true;
+    }
+    public void Remove(Item item)
+    {
+        items.Remove(item);
+        if (onItemChangedCallback != null)
+            onItemChangedCallback.Invoke();
+    }
+}
+
