6690a627e55c5ca331817282268f7aa0c5cabea0
ScorchedTrace <Songkhla0@gmail.com>
2022-03-07 13:38:32 +0700

    "please merge and commit"

---
+++ Lunar Hunt/Assets/Scripts/Dialogue/ConversationObject.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "Dialogue/ConversationObject")]
+public class ConversationObject : ScriptableObject
+{
+    //[SerializeField] private SpeakerObject speakerObject;
+    [SerializeField] private Dialogue[] dialogue;
+    [SerializeField] private ConversationObject changeConversation;
+    [SerializeField] private Response[] responses;
+    [SerializeField] private EventObject[] gameEvents;
+    [SerializeField] private Item[] items;
+    [SerializeField] private bool skipNotification;
+
+    //public string Speaker => speakerObject.name;
+    //public Sprite Portrait => speakerObject.portrait;
+    public Dialogue[] Dialogues => dialogue;
+    public bool HasChangeConversation => changeConversation != null;
+    public ConversationObject ChangeConversation => changeConversation;
+    public bool HasResponses => Responses != null && Responses.Length > 0;
+    public Response[] Responses => responses;
+    public bool HasEvents => gameEvents != null && gameEvents.Length > 0;
+    public EventObject[] GameEvents => gameEvents;
+    public bool HasItem => items != null;
+    public Item[] Items => items;
+    public bool SkipNotification => skipNotification;
+
+}
---
+++ Lunar Hunt/Assets/Scripts/Dialogue/ResponseHandler.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ResponseHandler : MonoBehaviour
+{
+    private DialogueUI dialogueUI;
+    private ResponseEvent[] responseEvents;
+    [SerializeField] private EventTracking eventTracking;
+
+    [SerializeField] private RectTransform responseBox;
+    [SerializeField] private RectTransform responseButtonTemplate;
+    [SerializeField] private RectTransform responseContainer;
+
+
+    private List<GameObject> tempResponseButtons = new List<GameObject>();
+    private float responseSpacing;
+
+    private void Start()
+    {
+        dialogueUI = GetComponent<DialogueUI>();
+
+        responseSpacing = responseContainer.GetComponent<VerticalLayoutGroup>().spacing;
+    }
+
+    public void AddResponseEvents(ResponseEvent[] responseEvents)
+    {
+        this.responseEvents = responseEvents;
+    }
+
+    public void ShowResponses(Response[] responses)
+    {
+        float responseBoxHeight = 0;
+
+        for (int i = 0; i < responses.Length; i++)
+        {
+            if (responses[i].RequireEvent == null || eventTracking.eventObjects.Contains(responses[i].RequireEvent))
+            {
+                Response response = responses[i];
+                int responseIndex = i;
+                float addSpacing = 0;
+                if (i > 0)
+                    addSpacing = responseSpacing;
+
+                GameObject responseButton = Instantiate(responseButtonTemplate.gameObject, responseContainer);
+                responseButton.gameObject.SetActive(true);
+                //responseButton.GetComponent<TMP_Text>().text = response.ResponseText;
+                responseButton.GetComponentInChildren<TMP_Text>().text = response.ResponseText;
+                responseButton.GetComponent<Button>().onClick.AddListener(call: () => OnPickedResponse(response, responseIndex));
+
+                tempResponseButtons.Add(responseButton);
+                responseBoxHeight += responseButtonTemplate.sizeDelta.y + addSpacing;
+            }
+            else
+            {
+                continue;
+            }
+        }
+
+        responseBox.sizeDelta = new Vector2(responseBox.sizeDelta.x, y: responseBoxHeight);
+        responseBox.gameObject.SetActive(true);
+    }
+
+    private void OnPickedResponse(Response response, int responseIndex)
+    {
+        responseBox.gameObject.SetActive(false);
+
+        //destroy choices button
+        foreach (GameObject button in tempResponseButtons)
+        {
+            Destroy(button);
+        }
+        tempResponseButtons.Clear();
+
+        //if there is response, do something? or what is this?
+        if (responseEvents != null && responseIndex <= responseEvents.Length)
+        {
+            responseEvents[responseIndex].OnPickedResponse?.Invoke();
+        }
+        responseEvents = null;
+
+        //If response has ConversationObject, use it
+        if (response.ConversationObject)
+        {
+            dialogueUI.runConversation(response.ConversationObject);
+        }
+        else
+        {
+            dialogueUI.CloseDialogueBox();
+        }
+
+        //dialogueUI.runConversation(response.ConversationObject);
+    }
+
+    public bool checkResponsesOption(Response[] responses)
+    {
+        int disabledResponse = 0;
+        for (int i = 0; i < responses.Length; i++)
+        {
+            if (responses[i].RequireEvent != null && !eventTracking.eventObjects.Contains(responses[i].RequireEvent))
+            {
+                disabledResponse += 1;
+            }
+        }
+        if (disabledResponse >= responses.Length)
+        {
+            Debug.Log("no responses to choose!");
+            return false;
+        }
+        else
+        {
+            Debug.Log("has responses to choose!");
+            return true;
+        }
+    }
+}
